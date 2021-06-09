6893a0e79bcf24dea869de80c88bdac21e22e77e
xayon40-12 <touroux.nathan@gmail.com>
2021-06-09 11:12:08 +0200

    compute sqr inside type

--- src/Once.rs
+++ src/Once.rs
@@ -1,41 +1,37 @@
 use crate::List::{self,*};
 use crate::*;
+use std::ops::{Mul,Add};
+use std::cmp::Ord;
 
-pub fn once<'a>(d: usize, m: usize) -> List<'a,List<'a,usize>> {
+pub fn once<'a>(d: usize, m: usize) -> List<'a,(List<'a,usize>,usize)> {
     match d {
-        0 => List::from_iter((0..=m).map(|_| list![]) ),
-        1 => List::from_iter((0..=m).map(|i| list![i])),
-        _ => compact(0,List::from_iter((0..=m).map(move |i|
-              List::from_iter(once(d-1,i).into_iter().map(move |j| Cons(i,lazy!(j)))))
+        0 => List::from_iter((0..=m).map(|_| (list![],0)) ),
+        1 => List::from_iter((0..=m).map(|i| (list![i],i*i))),
+        _ => compact(List::from_iter((0..=m).map(move |i|
+              List::from_iter(once(d-1,i).into_iter().map(move |(lj,j)| (Cons(i,lazy!(lj)),j+i*i) )))
             )),
     }
 }
 
-use std::ops::{Mul,Add};
-pub fn sqr<'a, N: Mul<Output=N> + Add<Output=N> + Copy>(s: N, l: List<'a,N>) -> N {
-    l.into_iter().fold(s, |a,i| a + i*i)
-}
-
-pub fn compact<'a, N: 'a + Ord + Mul<Output=N> + Add<Output=N> + Copy>(s: N, aaa: List<'a,List<'a,List<'a,N>>>) -> List<'a,List<'a,N>> {
+pub fn compact<'a, N: 'a + Ord + Mul<Output=N> + Add<Output=N> + Copy>(aaa: List<'a,List<'a,(List<'a,N>,N)>>) -> List<'a,(List<'a,N>,N)> {
     match aaa {
         Nil => Nil,
         Cons(aa,mut aaar) => match aa {
-            Nil => compact(s, aaar.val()),
-            Cons(a,aar) => Cons(a,lazy!(fuse(s.clone(), aar.clone().val(),compact(s.clone(), aaar.clone().val())))),
+            Nil => compact(aaar.val()),
+            Cons(a,aar) => Cons(a,lazy!(fuse(aar.clone().val(),compact(aaar.clone().val())))),
         }
     }
 }
 
-use std::cmp::Ord;
-pub fn fuse<'a, N: 'a + Ord + Mul<Output=N> + Add<Output=N> + Copy>(s: N, aa: List<'a,List<'a,N>>,bb: List<'a,List<'a,N>>) -> List<'a,List<'a,N>> {
+pub fn fuse<'a, N: 'a + Ord + Mul<Output=N> + Add<Output=N> + Copy>(aa: List<'a,(List<'a,N>,N)>,bb: List<'a,(List<'a,N>,N)>) -> List<'a,(List<'a,N>,N)> {
     match aa {
         Nil => bb,
-        Cons(a,ar) => match bb {
-            Nil => Cons(a,ar),
-            Cons(b,br) => if sqr(s,a.clone())<=sqr(s,b.clone()) {
-                Cons(a,lazy!(fuse(s,ar.clone().val(),Cons(b.clone(),br.clone()))))
+        Cons((av,a),ar) => match bb {
+            Nil => Cons((av,a),ar),
+            Cons((bv,b),br) => if a<=b {
+                Cons((av,a),lazy!(fuse(ar.clone().val(),Cons((bv.clone(),b.clone()),br.clone()))))
             } else {
-                Cons(b,lazy!(fuse(s,Cons(a.clone(),ar.clone()),br.clone().val())))
+                Cons((bv,b),lazy!(fuse(Cons((av.clone(),a.clone()),ar.clone()),br.clone().val())))
             }
         }
     }
@@ -47,13 +43,13 @@ mod tests {
     use crate::Once::*;
     #[test]
     fn test_once() {
-        println!{"{:?}", once(0,10).into_iter().map(Vec::from).collect::<Vec<Vec<_>>>()};
-        println!{"{:?}", once(1,10).into_iter().map(Vec::from).collect::<Vec<Vec<_>>>()};
-        println!{"{:?}", once(2,10).into_iter().map(Vec::from).collect::<Vec<Vec<_>>>()};
-        println!{"{:?}", once(3,10000).into_iter().take(20).map(Vec::from).collect::<Vec<Vec<_>>>()};
-        println!("{}", sqr(0, list![1,2,3]));
-        let l1 = List::from_iter((0..10).map(|i| list![i]));
-        let l2 = List::from_iter((0..10).map(|i| list![i*2]));
-        println!("{:?}", fuse(0,l1,l2).map(Vec::from).into_iter().collect::<Vec<_>>());
+        let f = |(av,a)| (Vec::from(av),a);
+        println!{"{:?}", once(0,10).into_iter().map(f).collect::<Vec<_>>()};
+        println!{"{:?}", once(1,10).into_iter().map(f).collect::<Vec<_>>()};
+        println!{"{:?}", once(2,10).into_iter().map(f).collect::<Vec<_>>()};
+        println!{"{:?}", once(3,10000).into_iter().take(20).map(f).collect::<Vec<_>>()};
+        let l1 = List::from_iter((0..10).map(|i| (list![i],i*i)));
+        let l2 = List::from_iter((0..10).map(|i| { let i = i*2; (list![i],i*i)}));
+        println!("{:?}", fuse(l1,l2).map(f).into_iter().collect::<Vec<_>>());
     }
 }
